cd7c156d9d89bddecf4bca8293fedb2e09524026
anmol <anmolof10gms@gmail.com>
2024-04-18 12:16:44 +0530

    updated the code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const path = require('path');
 
 
-const app = express(); 
+const app = express();
 
 const Shoe = require("./models/shoes");
 const upload = require("./middlewares/uploadImg");
@@ -16,15 +16,17 @@ const port = process.env.PORT || 3000; // Default to 3000 if PORT is not set
 
 // getting-started.js
 const mongoose = require('mongoose');
+const { error } = require("console");
 
 main()
-.then(() => console.log("mongodb connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("mongodb connected"))
+  .catch(err => console.log(err));
 
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/practice');
 }
+
 
 
 // view engine setup
@@ -35,25 +37,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
+
+
+app.get("/", (req, res) => {
+  res.send("<h1> This is home page </h1>");
+})
+
 app.get("/shoes", (req, res) => {
-return res.render("listShoes");
+  return res.render("listShoes");
 });
 
 
-app.post("/shoes",upload , async (req, res) => {
+app.post("/shoes", upload, async (req, res) => {
   let { price, discount } = req.body;
+if(req.uploadError) {
+  res.status(404).render("listShoes", {
+    uploadError: req.uploadError,
+  })
+} else {
+  console.log("no any error");
   const uploadedFiles = req.files;
+  const imageUrls = uploadedFiles.map(file => `/public/img/products/${file.filename}`); // Create image URLs
+  let discountPrice = Math.floor((price - ((discount / 100) * price))) || price;
+  let result = await Shoe.create({ ...req.body, discountPrice, images: imageUrls });
+  res.redirect("/");
+}
 
-const imageUrls = uploadedFiles.map(file => `/public/img/uploads/${file.filename}`); // Create image URLs
-  let discountPrice = Math.floor((price-((discount/100)*price)));
-  // console.log(`price: ${price}, discount: ${discount}%, discountPrice: ${discountPrice}, imageUrls: ${imageUrls}`);
-let result = await Shoe.create({...req.body, discountPrice, images: imageUrls });
-res.send("success");
 });
-
-
 
 
 app.listen(port, () => {
-    console.log(`server started and app is listening at port: ${port}`);
+  console.log(`server started and app is listening at port: ${port}`);
 });
--- middlewares/uploadImg.js
+++ middlewares/uploadImg.js
@@ -1,5 +1,16 @@
 const multer = require('multer');
 
+
+// validation
+const validateImage = (req, file, cb) => {
+  const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpeg'];
+  if (!allowedMimeTypes.includes(file.mimetype)) {
+    req.uploadError = new Error("only jpeg, jpg, png formats are allowed");
+    return cb(null, false);
+  }
+  cb(null, true); // Accept the file if valid
+};
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './public/img/uploads/')
@@ -10,7 +21,7 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage: storage }).array('images', 10); // Accept up to 10 images in an 'images' field
+const upload = multer({ storage: storage, fileFilter: validateImage }).array('images', 10); // Accept up to 10 images in an 'images' field
 // console.log(upload);
 
 module.exports = upload;
